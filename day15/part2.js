45ecfb33fcab0fe6a8f77e41be52bf3cdffce042
David Lozzi <lozzi.david@bcg.com>
2020-12-15 16:25:18 -0500

    updates day 15 part 2 to use a Map

--- day15/part2.js
+++ day15/part2.js
@@ -1,17 +1,17 @@
 var nums = [5,2,8,16,18,0,1]
-var numIndexes = {}
+var numIndexes = new Map()
 
 console.log(new Date())
-nums.forEach((n, i) => numIndexes[n] = i)
+nums.forEach((n, i) => numIndexes.set(n, i))
 
 let theNum = 0
 for(let i = nums.length; i <= 30000000 - 2; i += 1) {
-  if(numIndexes[theNum] >= 0) {
-    var lastIndex = numIndexes[theNum]
-    numIndexes[theNum] = i
+  if(numIndexes.has(theNum)) {
+    var lastIndex = numIndexes.get(theNum)
+    numIndexes.set(theNum, i)
     theNum = i - lastIndex
   } else {
-    numIndexes[theNum] = i
+    numIndexes.set(theNum, i)
     theNum = 0
   }
   if(i % 1000000 === 0) console.log(new Date(), i)

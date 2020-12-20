033db1d83c69b910ff886d5f04b6d55eb9d3be38
David Lozzi <lozzi.david@bcg.com>
2020-12-20 09:11:40 -0500

    adds day 14

--- day14/part2.js
+++ day14/part2.js
@@ -1,34 +1,51 @@
 const fs = require('fs');
-const sets = fs.readFileSync('./input.txt', 'utf-8').trim().split('\n');
+const set = fs.readFileSync('./input.txt', 'utf-8').trim().split('\n');
 
 let mask = '';
 let memory = [];
-let addresses = []
 
-const getAddress = mem => parseInt(mem.replace(/[^\d]/g, ''));
+const getAddr = m => parseInt(m.replace(/[^\d]/g, ''));
 
-const applyMask = decimal => {
-	// Convert the value to 36bit string
-	const binaryValue = parseInt(decimal).toString(2).padStart(36, '0');
-	
-	return parseInt(mask                            // Take the mask
-		.split('')                                    // Look at every bit
-		.map(b => parseInt(b))                        // Run it through a parseInt(), if it's 1, or 0, return it, X will throw NaN
-		.map((b, i) => isNaN(b) ? binaryValue[i] : b) // for each (b)it, if it's NaN return the bit in the slot vv[i], otherwise, b 
-		.join(''), 2);                                // smash it back together, convert to number, \o/
+const applyVals = (addr, v) => {
+	let maskBits = mask.split('');
+	let numAddrs = maskBits.filter(b => b === 'X').length;
+	let p = 0;
+	let xplace = 0;
+	let addrBits = addr.toString(2).padStart(36, '0').split('');
+
+	// For every element we want to set mem, 
+	// we are going to set however many Xs are in the mask
+	// Iterate over each num, build the binary representation, (p)
+	// place them in the X places (xplace).
+	for (let i = 0; i < Math.pow(2, numAddrs); i++) {
+		xplace = 0;
+		p = i.toString(2).padStart(numAddrs, '0').split('');
+
+		let memAddr = parseInt(maskBits.map((b, j) => {
+			if (b === '1') { return '1'; }
+			if (b === '0') { return addrBits[j]; }
+			if (b === 'X') { return p[xplace++]; }
+		}).map(b => parseInt(b)).join(''), 2);
+
+		// since this array will get larger than
+		// 2^32 elements, using a string key hash 
+		// will guarantee we don't lose any elements
+		memory[memAddr.toString()] = v;
+	}
 }
 
-
-sets.forEach(set => {
+set.forEach(set => {
 	let [instruction, value] = set.split(' = ');
 
 	if (instruction === 'mask') {
 		mask = value;
 	}
 	if (instruction.match(/mem/)) {
-    var address = getAddress(instruction)
-		memory[address] = applyMask(address);
+		applyVals(getAddr(instruction), parseInt(value));
 	}
 });
 
-console.log('PART 1', memory.reduce((a, b) => a + b, 0));
+let sum = 0;
+for (let i in memory) sum += memory[i];
+
+console.log('PART 2:', sum);

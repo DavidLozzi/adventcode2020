a183096dcdc4587becab3441e4ef84371adb5d45
David Lozzi <lozzi.david@bcg.com>
2020-12-25 13:55:48 -0500

    adds day 25

---
+++ day25/part1.js
@@ -0,0 +1,31 @@
+
+const card = 15628416
+let cardloopSize = 0
+let answer = 1
+while(answer !== card) {
+  answer = answer * 7
+  answer = answer % 20201227
+  cardloopSize += 1
+}
+
+console.log(cardloopSize, 'card loopsize')
+
+
+const door = 11161639
+let doorloopSize = 0
+answer = 1
+while(answer !== door) {
+  answer = answer * 7
+  answer = answer % 20201227
+  doorloopSize += 1
+}
+
+console.log(doorloopSize, 'card loopsize')
+
+let encryption = 1
+for(let i = 0; i < cardloopSize; i ++){
+  encryption = encryption * door
+  encryption = encryption % 20201227
+}
+
+console.log(encryption, 'encryption')
